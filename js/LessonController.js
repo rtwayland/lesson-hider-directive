d787437f86a1583290619c8727e3416172da81af
raleigh wayland <rtwayland@gmail.com>
2017-01-24 15:06:04 -0700

    Delete functionality

--- js/LessonController.js
+++ js/LessonController.js
@@ -1,4 +1,12 @@
 angular.module('directivePractice')
     .controller('LessonController', function($scope, LessonService) {
         $scope.lessons = ['Services', 'Routing', 'Directives', 'Review', 'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
+
+        $scope.announceDay = function(lesson, day) {
+          if (day) {
+            alert(lesson + ' lesson was taught on ' + day + '.');
+          } else {
+            alert(lesson + ' has not been scheduled yet.');
+          }
+        };
     });
--- js/lessonHiderDirective.js
+++ js/lessonHiderDirective.js
@@ -4,21 +4,30 @@ angular.module('directivePractice')
             restrict: 'E',
             templateUrl: 'lessonHider.html',
             scope: {
-                lesson: '='
+                lesson: '=',
+                dayAlert: '&'
             },
             link: function(scope, elem, attrs) {
                 scope.getSchedule.then(function(response) {
-                    scope.schedule = response.data;
                     for (var i = 0; i < scope.schedule.length; i++) {
                         if (scope.schedule[i].lesson === scope.lesson) {
+                            scope.lessonDay = scope.schedule[i].weekday;
                             elem.css('text-decoration', 'line-through');
                             return;
                         }
                     }
                 });
             },
-            controller: function($scope, LessonService) {
-                $scope.getSchedule = LessonService.getSchedule();
+            controller: function($scope, LessonService, $element) {
+                $scope.getSchedule = LessonService.getSchedule()
+                    .then(function(response) {
+                        $scope.schedule = response.data;
+                    });
+
+                $scope.deleteLesson = function() {
+                    $element.remove();
+                    $scope.$destroy();
+                };
             }
         };
     });
